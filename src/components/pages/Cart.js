73483e2a19bd26602bb34167e79f1ac0a672c576
ugbeadie <ugbeadie3@gmail.com>
2022-08-18 16:34:04 +0100

    add "remove from cart" feature

--- src/components/pages/Cart.js
+++ src/components/pages/Cart.js
@@ -1,26 +1,45 @@
 import React from 'react'
-// import Products from '../Products'
 
 const Cart = ({cart,setCart}) => {
+
+  const handleRemove = (id) => {
+    const arr = cart.filter((item) => item.id !== id)
+    setCart(arr)
+  }
+
   return (
     <div className='cart'>
-      <div className='cart-header'>Your Cart</div>
+      <div className='cart-header'>Shopping Cart</div>
       {cart.length === 0 && (
         <div>
           Your Cart Is Empty
         </div>
       )}
       <div className='cart-items'>
-        {cart.map((item) => 
-        <div key={item.id}>
-          <div className='cart-box'>
-            <img className='item-img' src={item.imageFront} alt={item.name}/>
-            <div class="detail-box">
-                <div class="item-name">{item.name}</div>
-                
+      <div className='cart-item-info'>
+        <p>PRODUCT</p>
+        <p>PRICE</p>
+        <p>QUANTITY</p>
+        <p>SUBTOTAL</p>
+      </div>
+      {cart.map((item) => 
+        <div key={item.id} className='cart-box'>
+            <div className='col-1'>
+              <img className='item-img' src={item.imageFront} alt={item.name}/>
+              <div>
+                <p>{item.name}</p>
+                <h6 className='cart-remove' 
+                onClick={() => handleRemove(item.id)}>Remove</h6>
+              </div>
             </div>
+            <div className='col-2'>{item.price}</div>
+            <div className='col-3'>
+              <button className='minus'>-</button>
+              <span>1</span>
+              <button className='plus'>+</button>
+            </div>        
+            <div className='col-4'>{item.price}</div>
           </div>
-        </div>
         )}
       </div>
     </div>
